88dba9c3a6b710a35984dbd529516edf66afeb66
Amanda <amandaoliveirapolari@gmail.com>
2023-02-13 22:05:50 -0300

    Exercício 1 -> ok!

---
+++ exercicio1/script.js
@@ -0,0 +1,16 @@
+// EXERCÍCIO 1
+// Array somente com números:
+const listaDeNumeros = [1990, 1995, 27, 31, 2018];
+
+// Array somente com strings:
+const listaDePalavras = ['Amanda', 'Luan', 'Julia', 'Eternidade'];
+
+// Array com números, strings e booleanos:
+const listaDiversificada = [7, 15, 'Amor', 'Superação', true, false];
+
+// Imprimindo todas as arrays no console:
+console.log(
+    'Array com números', listaDeNumeros, '\n',
+    'Arrays com strings', listaDePalavras, '\n',
+    'Array diversificada', listaDiversificada
+)
